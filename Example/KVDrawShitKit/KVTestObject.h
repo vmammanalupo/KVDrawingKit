2dad90e39098116bf14fc017dbaa43d822384567
yvxm026 <Vince.Mammana-Lupo@grainger.com>
2016-04-16 16:35:46 -0500

    Test Object

---
+++ Example/KVDrawShitKit/KVTestObject.h
@@ -0,0 +1,13 @@
+//
+//  KVTestObject.h
+//  
+//
+//  Created by Mammana-Lupo, Vince on 4/16/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KVTestObject : NSObject
+
+@end
---
+++ Example/KVDrawShitKit/KVTestObject.m
@@ -0,0 +1,13 @@
+//
+//  KVTestObject.m
+//  
+//
+//  Created by Mammana-Lupo, Vince on 4/16/16.
+//
+//
+
+#import "KVTestObject.h"
+
+@implementation KVTestObject
+
+@end
